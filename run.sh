96397f3618a2d406c11ed0b14271e04e43ff773f
Gilles Habran <63115776+gillouche@users.noreply.github.com>
2022-05-24 19:13:03 +0200

    Specify become when needed (#2)
    Specify become when needed

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ else
     verbosity=""
 fi
 
-TARGET_USER=$USER
-sudo su -c "ansible-playbook -i hosts.yml "playbook.yml" --extra-vars 'target_user=$TARGET_USER' ${verbosity}"
+ansible-playbook -i hosts.yml site.yml --ask-become-pass -e target_user=$USER ${verbosity}
